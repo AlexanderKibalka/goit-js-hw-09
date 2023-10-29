663a20cbdcf7716692d1cf8212d17c80b4d60138
Alex <alexandrkibalka@gmail.com>
2023-10-29 19:23:20 +0200

    hw1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startBtn = document.querySelector(".js-start");
+const stopBtn = document.querySelector(".js-stop");
+const body = document.querySelector('body');
 
+let timerId = null;
+
+startBtn.addEventListener("click", (evt) => {
+    evt.target.disabled = true;
+  timerId = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+});
