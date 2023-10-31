2f7e269c1ae7f9c71702a7eee04b2a0ad2370aa0
Alex <alexandrkibalka@gmail.com>
2023-10-31 08:53:02 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,34 @@
+const form = document.querySelector('form');
+form.addEventListener('submit', onSubmitForm);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((res, rej) => {
+    var shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+
+  let delay = Number(form.elements.delay.value);
+  let step = Number(form.elements.step.value);
+  let amount = Number(form.elements.amount.value);
+  let promiseDelay = delay;
+  for (let i = 1; i <= amount; i += 1) {
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    promiseDelay = delay + step * i;
   }
-}
+};
